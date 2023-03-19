cfb649e455239819d7b8027ee80fbb621581bfe8
tutu.gafarli <tutu.gafarli@mpay.az>
2023-03-19 15:56:19 +0400

    Initial commit

---
+++ src/test/java/com/example/foodsapi/FoodsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
